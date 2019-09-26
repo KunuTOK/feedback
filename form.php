9ab3e89e025a941cab866423ff74e3ad22d822bf
KunuTOK <kunutok@mail.ru>
2019-09-27 00:08:53 +0300

    после установки sudo apt-get install sendmail зарузка формы составила 3 минуты

--- form.php
+++ form.php
@@ -10,17 +10,28 @@
 <?php
 $back = "<p><a href=\"javascript: history.back()\">Вернуться назад</a></p>";
 
-if (!empty($_POST['a']) and !empty($_POST['b']) and !empty($_POST['c']) and !empty($_POST['d'])
-    and !empty($_POST['e'])) {
-    $a = trim(strip_tags($_POST['a']));
-    $b = trim(strip_tags($_POST['b']));
-    $c = trim(strip_tags($_POST['c']));
-    $d = trim(strip_tags($_POST['d']));
-    $e = trim(strip_tags($_POST['e']));
+$connection = mysqli_connect('127.0.0.1','admin','123','fb_db');
+      if($connection == false)
+      {
+          echo "error! <br>";
+          echo mysqli_connect_error();
+          exit;
+      }
 
-    mail('kunutok@gmail.com', 'Письмо с адрес_вашего_сайта',
-        'Вам написал: ' . $a . $b . $c . '<br />Его почта: ' . $d . '<br />
-      Его сообщение: ' . $e, "Content-type:text/html;charset=windows-1251");
+
+if (!empty($_POST['familia']) and !empty($_POST['name']) and !empty($_POST['patronymic']) and !empty($_POST['email'])
+    and !empty($_POST['mes'])) {
+    $familia = trim(strip_tags($_POST['familia']));
+    $name = trim(strip_tags($_POST['name']));
+    $patronymic = trim(strip_tags($_POST['patronymic']));
+    $email = trim(strip_tags($_POST['email']));
+    $mes = trim(strip_tags($_POST['mes']));
+
+    mail('kunutok@gmail.com', 'Письмо с fedback.github.io',
+        'Вам написал: ' . $familia . $name . $patronymic . '<br />Его почта: ' . $email . '<br />
+      Его сообщение: ' . $mes, "Content-type:text/html;charset=windows-1251");
+      
+    mysqli_query($connection,"INSERT INTO `fb` (`familiya`, `Name`, `patronymic`, `email`, `message`) VALUES ('$familia', '$name', '$patronymic', '$email', '$mes')");
 
     echo "Ваше сообщение успешно отправлено!<Br> Вы получите ответ в
       ближайшее время<Br> $back";
