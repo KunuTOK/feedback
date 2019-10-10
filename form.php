503e5ffc13c087eb048ceb86f7f9397087c1b05a
KunuTOK <kunutok@mail.ru>
2019-10-10 22:12:38 +0300

    добавил скрипт на проверку email`a и AJAX форму в form.php.  на тестовом задании сказали проверка формы по email`у и блокировка должна проходить через AJAX. не прочувствовал. смысл один опрашиваем форму есть там такой мэйл или нет и закрываем форму. чем хуже просто заурытие если вывели ошибку заполнения?

--- form.php
+++ form.php
@@ -15,11 +15,6 @@ if ($connection == false) {
     exit;
 }
 
-if (isset($_COOKIE[‘FormSubmitted’]))
-{
-die("Вы может отправить форму только один раз за сессию!");
-}
-
 if (!empty($_POST['familia']) and !empty($_POST['name']) and !empty($_POST['patronymic']) and !empty($_POST['email'])
     and !empty($_POST['message'])) {
     $familia = trim(strip_tags($_POST['familia']));
@@ -28,21 +23,27 @@ if (!empty($_POST['familia']) and !empty($_POST['name']) and !empty($_POST['patr
     $email = trim(strip_tags($_POST['email']));
     $message = trim(strip_tags($_POST['message']));
 
+    if(isset($_POST['email']) && $_POST['email'] != ''){
+        $email2 = $_POST['email'];
+        $email1 = mysqli_real_escape_string($connection,$email2);
+        if(!filter_var($email1, FILTER_VALIDATE_EMAIL)){
+          echo 'invalid';
+        }else{
+          $sql = "SELECT * FROM fb WHERE email='$email2'";
+          $result1 = mysqli_query($connection,$sql);
+          if(mysqli_num_rows($result1) == 1){
+            die ("Сообщение с почтой  $emails уже отправлено <Br> $back");
+          }
+        }
+      }
+
     mail('kunutok@gmail.com', 'Письмо с fedback.github.io',
         'Вам написал: ' . $familia . $name . $patronymic . '<br />Его почта: ' . $email . '<br />
   Его сообщение: ' . $message, "Content-type:text/html, charset=utf-8");
-
-  setcookie(‘FormSubmitted’, $email);
  
     mysqli_query($connection, "INSERT INTO `fb` (`familiya`, `name`, `patronymic`, `email`, `message`) VALUES ('$familia', '$name', '$patronymic', '$email', '$message')");
-
-    $result = mysqli_affected_rows($connection);
-    if ($result > 0) {
         echo " Сообщение отправлено. Спасибо <Br> $back";
         exit;
-    } else {
-        echo "письмо с почтой  $email уже отправлено <Br> $back";
- }
 } else {
     echo "Для отправки сообщения заполните все поля! $back";
     exit;
